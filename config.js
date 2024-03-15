a2f4c7765064a88206610e9f8ff08aac27306190
cinoxdz <125744743+cinoxdz@users.noreply.github.com>
2024-03-15 16:11:07 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  bot: {
+    owners: [""],  // ايدي الاونر
+    botID: "",    // ايدي البوت
+    GuildId: "",   // ايدي السيرفير
+    ClientId: "",    // ايدي البوت
+    serverinvte: "", // انفايت سيرفر
+    clientSECRET: process.env.client , // سكريت
+    callbackURL: "/login", // الكال باك
+    inviteBotUrl: "",// انفايت البوت
+    TheLinkVerfy : '', // رابط اوثو رايز بالصلاحيه ادخال الي سيرفرات
+    prefix : '$', 
+    ceatogry : '', // كاتوجري الي يفتح فيها تكت شراء
+     TOKEN: (process.env.midd),// توكن 
+    Price: 1,    // سعر العضو الواحد
+    TraId  : '' // ايدي الي يتحوله كريديت
+  },
+  website: {
+    PORT: "3001",
+  }
+}
